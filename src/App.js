5906b301ee29f0501c4b246d666ecf1188397816
brameldering <bram.eldering@gmail.com>
2023-04-19 15:45:09 +0200

    Get github data using fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
+function GitHubUser({ name, location, avatar }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt={name} />
     </div>
   );
+}
+
+function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://api.github.com/users/brameldering`)
+      .then((response) => response.json())
+      .then(setData)
+      .then(() => setIsLoading(false))
+      .catch(setError);
+  }, []);
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+  if (!data) return null;
+
+  return <GitHubUser name={data.name} location={data.location} avatar={data.avatar_url} />;
 }
 
 export default App;
